2345f9c55c2ec7b9e95bbad4a99ec35eccd90641
alexvb92 <a_vonbredow@live.com>
2016-10-06 19:25:47 -0400

    two ports on server.js

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ var http = require('http');
 // define port we want to listen to
 var PORT1 = 7000;
 var PORT2 = 7500;
+var good = ['nice', 'cool', 'awesome']
+var bad = ['ungood', 'not ncie', 'unawesome']
 
-// function to handle requests and send respense
-function handleRequest (request , response) {
-	response.end('We made it everyone! This was your URL: ' + request.url)
+function handleRequest1 (request , response) {
+	response.end('very' + good[Math.floor(Math.random() * good.length)])
 }
-
+function handleRequest2 (request , response) {
+	response.end('very' + bad[Math.floor(Math.random() * bad.length)])
+}
 // create a server
-var server1 = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
 
-var server2 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
 
 // start our server
